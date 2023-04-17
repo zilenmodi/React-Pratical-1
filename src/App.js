5b1aa76bf0b121893e4369eb77101af13a4d976c
zilenmodi <zilen.m@simformsolutions.com>
2023-04-17 15:47:09 +0530

    Add readme file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css'
-import xyz from "./assets/download.png"
+import image from "./assets/download.png"
 
 const App = () => {
     return (
         <>
             <h1>Hello World!</h1>
-            <img src={xyz} />
+            <img src={image} />
         </>
     )
 }
