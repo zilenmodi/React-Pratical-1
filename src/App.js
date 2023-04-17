d0719416fb6213bd7f04d05a5b07b5e029278a74
zilenmodi <zilen.m@simformsolutions.com>
2023-04-17 15:25:13 +0530

    Create react app using webpack.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'
+import xyz from "./assets/download.png"
+
+const App = () => {
+    return (
+        <>
+            <h1>Hello World!</h1>
+            <img src={xyz} />
+        </>
+    )
+}
+
+export default App
